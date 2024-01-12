abe0e9c03ba2848666dc0ec8d1ce2cbffbe3c19e
xiaxuanzhe <3058634081@qq.com>
2024-01-12 19:26:41 +0100

    init

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import {Nunito} from "next/font/google";
+import './globals.css'
+import Navbar from "./components/navbar/navbar";
+
+export const metadata = {
+  title: 'HelloFirst',
+  description: 'Hello First people',
+}
+
+const font = Nunito({
+  subsets: ['latin'],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className={font.className}>
+        <Navbar />
+        {children}
+       </body>
+    </html>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,js,tsx}",
+    "./components/**/*.{js,ts,js,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
